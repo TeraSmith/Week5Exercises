65ca0670d8cb10637b0fb17bb6a4d127e8b624af
TeraSmith <67851457+TeraSmith@users.noreply.github.com>
2020-08-04 06:28:20 -0700

    Add files via upload

---
+++ UserStrings.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class UserStrings {
+
+	public static void main(String[] args) throws IOException {
+		
+		Scanner strings = new Scanner(System.in);
+		System.out.println("Type DONE to terminate strings.");
+		
+	       PrintWriter pw = new PrintWriter(new File("userStrings.txt"));
+	       while (true){
+	           String user = strings.nextLine();
+	           if(user.equalsIgnoreCase("DONE")){
+	               pw.flush();
+	               pw.close();
+	               break;
+	           }
+	           pw.println(user);
+	       }
+	      
+	   }
+
+	}
